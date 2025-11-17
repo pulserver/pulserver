c67e47cb96d30cc5297aa610c8f4dc3e98429ec7
mcencini <matteo.cencini@gmail.com>
2025-11-17 17:28:32 +0100

    wip direct Pulserver reading prototype

--- src/pulserver/prototype/_methods.py
+++ src/pulserver/prototype/_methods.py
@@ -17,19 +17,23 @@ class pulserver_BlockGroup:
     def __init__(self):
         self.label = None
         self.start = None
-        self.end = None
+        self.size = None
 
 def pulserver_get_block_groups(sequences, num_blocks_per_tr):
     num_block_groups_per_tr = []
     block_group_labels = []
     block_group_starts = []
     block_group_ends = []
+    block_group_sizes = []
+    block_groups_table = []
     
     for n, seq in enumerate(sequences):
         num_block_groups_per_tr.append(0)
         block_group_labels.append([])
         block_group_starts.append([])
         block_group_ends.append([])
+        block_group_sizes.append([])
+        block_groups_table.append([])
         for b in range(num_blocks_per_tr[n]):
             block = seq.get_block(b+1)
             
@@ -44,16 +48,19 @@ def pulserver_get_block_groups(sequences, num_blocks_per_tr):
                     raise RuntimeError("First block in sequence is a parent block by definition and must have a label")
                 current_label = block_label
                 block_group_starts[n].append(b)
+                block_groups_table[n].append(block_label)
                 continue
             else:
                 block_label = current_label # default
                 if hasattr(block, 'label'):
                     for lbl in block.label.values():
                         if lbl.label == 'BLOCKID':
-                            block_label = lbl.value
+                            block_label = lbl.value            
             
             # Second to last block in TR
             if block_label != current_label:
+                block_groups_table[n].append(block_label)
+                
                 # If previous block is new, add its end and its label to the list
                 if current_label not in block_group_labels[n]:
                     block_group_labels[n].append(current_label)
@@ -72,7 +79,12 @@ def pulserver_get_block_groups(sequences, num_blocks_per_tr):
                 if current_label not in block_group_labels[n]:
                     block_group_labels[n].append(current_label)
                     block_group_ends[n].append(b+1)
-    
+                    
+    # Get Group sizes
+    for n, num_block_groups in enumerate(num_block_groups_per_tr):
+        for b in range(num_block_groups):
+            block_group_sizes[n].append(block_group_ends[n][b] - block_group_starts[n][b])
+
     # Transform to structs
     block_groups = []
     for n, num_block_groups in enumerate(num_block_groups_per_tr):
@@ -81,9 +93,9 @@ def pulserver_get_block_groups(sequences, num_blocks_per_tr):
             block_groups[n].append(pulserver_BlockGroup())
             block_groups[n][b].label = block_group_labels[n][b]
             block_groups[n][b].start = block_group_starts[n][b]
-            block_groups[n][b].end = block_group_ends[n][b]
+            block_groups[n][b].size = block_group_sizes[n][b]
             
-    return  block_groups   
+    return  block_groups, block_groups_table 
             
 # %% Planning stage
 
