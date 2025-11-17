b8d277fc51d299ab650aad7c871b842b51788c5e
mcencini <matteo.cencini@gmail.com>
2025-11-17 17:05:36 +0100

    prototyping direct pulserver struct readers in Python (before writing in C

---
+++ src/pulserver/prototype/__init__.py
@@ -0,0 +1,7 @@
+
+__all__ = []
+
+from ._methods import * # noqa
+from . import _methods
+
+__all__.extend(_methods.__all__)
---
+++ src/pulserver/prototype/_methods.py
@@ -0,0 +1,103 @@
+
+__all__ = ["pulserver_get_num_blocks_per_tr", "pulserver_get_block_groups"]
+
+#%% utils
+def pulserver_get_num_blocks_per_tr(sequences):
+    num_blocks_per_tr = []
+    for seq in sequences:
+        found, count = 0, 0
+        while found < 2:
+            b = seq.get_block(count+1)
+            found += hasattr(b, 'label') and 'TRID' in [lbl.label for lbl in b.label.values()]
+            count += 1
+        num_blocks_per_tr.append(count-1)
+    return num_blocks_per_tr
+
+class pulserver_BlockGroup:
+    def __init__(self):
+        self.label = None
+        self.start = None
+        self.end = None
+
+def pulserver_get_block_groups(sequences, num_blocks_per_tr):
+    num_block_groups_per_tr = []
+    block_group_labels = []
+    block_group_starts = []
+    block_group_ends = []
+    
+    for n, seq in enumerate(sequences):
+        num_block_groups_per_tr.append(0)
+        block_group_labels.append([])
+        block_group_starts.append([])
+        block_group_ends.append([])
+        for b in range(num_blocks_per_tr[n]):
+            block = seq.get_block(b+1)
+            
+            # First block
+            if b == 0:
+                num_block_groups_per_tr[n] += 1
+                block_label = None
+                for lbl in block.label.values():
+                    if lbl.label == 'BLOCKID':
+                        block_label = lbl.value
+                if block_label is None:
+                    raise RuntimeError("First block in sequence is a parent block by definition and must have a label")
+                current_label = block_label
+                block_group_starts[n].append(b)
+                continue
+            else:
+                block_label = current_label # default
+                if hasattr(block, 'label'):
+                    for lbl in block.label.values():
+                        if lbl.label == 'BLOCKID':
+                            block_label = lbl.value
+            
+            # Second to last block in TR
+            if block_label != current_label:
+                # If previous block is new, add its end and its label to the list
+                if current_label not in block_group_labels[n]:
+                    block_group_labels[n].append(current_label)
+                    block_group_ends[n].append(b)
+                    
+                # Update current block
+                current_label = block_label
+                
+                # If current block is new, add its start to the list
+                if current_label not in block_group_labels[n]:
+                    num_block_groups_per_tr[n] += 1
+                    block_group_starts[n].append(b)
+                    
+            # Last block in TR
+            if b == num_blocks_per_tr[n] - 1:
+                if current_label not in block_group_labels[n]:
+                    block_group_labels[n].append(current_label)
+                    block_group_ends[n].append(b+1)
+    
+    # Transform to structs
+    block_groups = []
+    for n, num_block_groups in enumerate(num_block_groups_per_tr):
+        block_groups.append([])
+        for b in range(num_block_groups):
+            block_groups[n].append(pulserver_BlockGroup())
+            block_groups[n][b].label = block_group_labels[n][b]
+            block_groups[n][b].start = block_group_starts[n][b]
+            block_groups[n][b].end = block_group_ends[n][b]
+            
+    return  block_groups   
+            
+# %% Planning stage
+
+# Pre-download
+# 1) Get echo filters
+def pulserver_get_adc(adc_library):
+    ...            
+    
+def pulserver_get_rf(sequences):
+    ...
+    
+# Pulsegen
+def pulserver_get_segments(sequences):
+    ...
+
+
+# %% Real Time stage
