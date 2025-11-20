eb43d07091e2b90b1f41e49c4bbc08bd009efd70
mcencini <matteo.cencini@gmail.com>
2025-11-20 17:05:21 +0100

    moving to file-based shapes library

--- csrc/pulseq/pulseqlib_methods.h
+++ csrc/pulseq/pulseqlib_methods.h
@@ -12,40 +12,14 @@
 
 /** 
  * Default ALLOC to malloc if it's not already defined
- *
- * Users are encouraged to replace with vendor-specific implementations if needed:
- * 
- * // pulseqlib_vendor_methods.h (vendor-specific header)
- * 
- * #include "my_vendor_library.h"  // This contains the definition of MyVendorAlloc
- * 
- * // Override ALLOC to use MyVendorAlloc in the vendor environment
- * #define ALLOC(size) MyVendorAlloc(size)  // Replaces malloc with MyVendorAlloc
- * 
- * #include "pulseqlib_methods.h"  // Now include the vendor-agnostic pulseqlib_vendor_methods.h with the overridden ALLOC
- * 
- * // Other vendor-specific declarations can go here
-*/
+ */
 #ifndef ALLOC
     #define ALLOC(size) malloc(size)
 #endif
 
 /** 
  * Default FREE to free if it's not already defined
- *
- * Users are encouraged to replace with vendor-specific implementations if needed:
- * 
- * // pulseqlib_vendor_methods.h (vendor-specific header)
- * 
- * #include "my_vendor_library.h"  // This contains the definition of MyVendorAlloc
- * 
- * // Override FREE to use MyVendorFree in the vendor environment
- * #define FREE(ptr) MyVendorFree(ptr)  // Replaces malloc with MyVendorAlloc
- * 
- * #include "pulseqlib_methods.h"  // Now include the vendor-agnostic pulseqlib_vendor_methods.h with the overridden FREE
- * 
- * // Other vendor-specific declarations can go here
-*/
+ */
 #ifndef FREE
   #define FREE(ptr) free(ptr)
 #endif
@@ -55,7 +29,7 @@ void pulseqlib_seqFileInit(const char* filePath, pulseqlib_SeqFile* seq);
 void pulseqlib_seqFileFree(pulseqlib_SeqFile* seq);
 void pulseqlib_seqFileReset(pulseqlib_SeqFile* seq);
 
-void pulseqlib_seqBlockInit(pulseqlib_SeqBlock* block);
+void pulseqlib_seqBlockInit(pulseqlib_SeqBlock* block); /* Reverted to simple signature */
 void pulseqlib_seqBlockFree(pulseqlib_SeqBlock* block);
 
 /* Parsing Sequence */
@@ -63,6 +37,6 @@ void pulseqlib_readSeq(pulseqlib_SeqFile* seq, const int readBlocks);
 
 /* Getters */
 void pulseqlib_getRawBlockContentIDs(const pulseqlib_SeqFile* seq, const int blockIndex, const int parseExtensions, pulseqlib_RawBlock* block);
-void pulseqlib_getBlock(const pulseqlib_SeqFile* seq, const int blockIndex, const int parseExtensions, pulseqlib_SeqBlock* block);
+void pulseqlib_getBlock(pulseqlib_SeqFile* seq, const int blockIndex, const int parseExtensions, pulseqlib_SeqBlock* block);
 
 #endif /* PULSEQLIB_METHODS_H */
